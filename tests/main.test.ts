0b4e0000f21035cc1638eb9c5616270deae143d4
Justin Joseph <justin.joseph0@yahoo.com>
2024-11-19 21:50:12 -0500

    Configure mock service worker library

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,14 @@
 import { it, expect, describe } from 'vitest';
 
+import { Category } from '../src/entities';
+
 describe('group', () => {
-  it('should', () => {
-    expect(1).toBeTruthy();
+  it('should', async () => {
+    const response = await fetch('/categories');
+
+    const categories = await response.json() as Category[];
+    console.log(categories);
+
+    expect(categories.length).toBe(3);
   });
 });
