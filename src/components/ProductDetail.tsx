4cfc5083687b7b0f042aa8175cb17f6d8d5d020b
Justin Joseph <justin.joseph0@yahoo.com>
2024-11-20 21:25:03 -0500

    Perform minor clean-up

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from "react-query";
-import { Product } from "../entities";
 import axios from "axios";
+import { useQuery } from "react-query";
+
+import { Product } from "../entities";
 
 const ProductDetail = ({ productId }: { productId: number }) => {
   const { isLoading, error, data: product } = useQuery<Product, Error, Product>({
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+
 import { Product } from "../entities";
 
 const ProductList = () => {
